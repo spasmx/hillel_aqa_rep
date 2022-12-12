fb5ac97da20b389a335ea10d0a3479b604e6d798
maksum <maksimlushchan@gmail.com>
2022-12-12 15:19:09 +0200

    Homework12.Recursion

---
+++ recursion_hw12.py
@@ -0,0 +1,16 @@
+# Write a function that takes an integer - number.
+# The function must return 'yes' if number is a power of 2, otherwise 'no'.
+# It is forbidden to use the exponentiation function or operator.
+
+def is_power_if_two(x, _count=1):
+    if not isinstance(x, int):
+        return f'Invalid data. {x} must be integer'
+
+    if x == 2:
+        return f'Yes\n2 in degree {_count} = {2 ** _count}'
+    if x == 1:
+        return 'No'
+
+    return is_power_if_two(int(x/2), _count + 1)
+
+
