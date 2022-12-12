8e21d818330188be56c3335c5a3e8a4c0d2a233a
maksum <maksimlushchan@gmail.com>
2022-12-12 15:39:04 +0200

    Homework12.Recursion

--- recursion_hw12.py
+++ recursion_hw12.py
@@ -4,13 +4,13 @@
 
 def is_power_if_two(x, _count=1):
     if not isinstance(x, int):
-        return f'Invalid data. {x} must be integer'
+        return f'Invalid argument. {x} must be integer'
 
     if x == 2:
         return f'Yes\n2 in degree {_count} = {2 ** _count}'
     if x == 1:
         return 'No'
 
-    return is_power_if_two(int(x/2), _count + 1)
+    return is_power_if_two(int(x/2), _count + 1) if x > 0 else 'Argument must be more than 0'
 
 
