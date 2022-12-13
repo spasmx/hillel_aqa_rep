5cb03c9ecb699cf667d0dc881c6f6d185cc003d8
maksum <maksimlushchan@gmail.com>
2022-12-13 16:57:40 +0200

    Homework11.Decorator

--- decorator_hw11.py
+++ decorator_hw11.py
@@ -1,42 +1,22 @@
 # Write a call_times decorator that will take a file_name as a parameter,
 # count the number of function calls and write to the file
 # in the format f'{func_name} has been called {count} times.\n'
+
 def call_times(file_name):
     def wrapper(func):
         def inner(*args, **kwargs):
+            func(*args, **kwargs)
             file1 = open(file_name, 'a+')
             file2 = open(file_name)
-            func(*args, **kwargs)
             count = file2.read().count(func.__name__) + 1
-            func_counts = {func.__name__: count}
-            log = f'{func.__name__} called {func_counts[func.__name__]} times.\n'
+            log = f'{func.__name__} called {count} times.\n'
+
             file1.write(log)
 
+            file2.close()
+            file1.close()
         return inner
     return wrapper
 
-
-
-
-
-@call_times('foo.txt')
-def foo():
-    pass
-
-@call_times('foo.txt')
-def boo():
-    pass
-
-@call_times('calls.txt')
-def doo():
-    pass
-
-print(foo())
-print(boo())
-print(foo())
-print(doo())
-print(doo())
-print(foo())
-print(foo())
 
 
