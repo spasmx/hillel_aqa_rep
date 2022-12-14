1a2e8766b26d4a0d84d2f737d33e6b934b1f1359
Maksum <spasmx@DESKTOP-6E5H7JF>
2022-12-14 12:42:54 +0200

    Homework11.Decorator

--- decorator_hw11.py
+++ decorator_hw11.py
@@ -1,25 +1,28 @@
 # Write a call_times decorator that will take a file_name as a parameter,
 # count the number of function calls and write to the file
 # in the format f'{func_name} has been called {count} times.\n'
-import shutil
+
+
+func_call_times = {}
+count = 0
+
 def call_times(file_name):
+
     def wrapper(func):
         def inner(*args, **kwargs):
+            global func_call_times
+            global count
             func(*args, **kwargs)
-            file1 = open('file.txt', 'a+')
-            file2 = open('file.txt')
-            count = file2.read().count(func.__name__) + 1
-            log = f'{func.__name__} called {count} times.\n'
-            file1.write(log)
-            file2.close()
-            file1.close()
-            shutil.copyfile('file.txt', file_name)
-            file3 = open(file_name, 'w')
-            file4 = open(file_name, 'a+')
-            file3.write(f'{func.__name__} called {count} times.\n')
-            if func.__name__ in file4.read():
-                file4.write(f'{func.__name__} called {count} times.\n')
+            if func.__name__ in func_call_times:
+                count += 1
+            else:
+                count = 1
+            func_call_times[func.__name__] = count
+            with open(file_name, 'w+') as f:
+                func(*args, **kwargs)
 
+                f.write(f'{func.__name__} be called {count} times.\n')
+            return f'{file_name} was update'
 
         return inner
     return wrapper
@@ -40,15 +43,29 @@ def boo():
 def doo():
     pass
 
-print(foo())
+
+print(boo())
 print(boo())
 print(boo())
 print(doo())
 print(doo())
+print(doo())
+print(doo())
+print(doo())
 print(foo())
 print(foo())
 print(foo())
 print(foo())
+print(foo())
+print(foo())
+print(foo())
+print(foo())
+print(foo())
+print(foo())
+print(foo())
+
+
+print(func_call_times)
 
 
 
